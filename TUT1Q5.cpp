a277bfb7420a0e04dd78bd69953af020208b886e
Yatish2012 <yatishgoyal27@gmail.com>
2025-09-03 00:00:37 +0530

    Update TUT1Q5.cpp

--- TUT1Q5.cpp
+++ TUT1Q5.cpp
@@ -1,24 +1,23 @@
-#include <stdio.h>
-
-void printRowColumnSum(int mat[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        int rowSum = 0, colSum = 0;
-        for (int j = 0; j < 3; j++) {
-            rowSum += mat[i][j];
-            colSum += mat[j][i];
-        }
-        printf("Row %d Sum = %d, Column %d Sum = %d\n", i + 1, rowSum, i + 1, colSum);
-    }
-}
-
-int main() {
-    int mat[3][3];
-    printf("\nEnter a 3x3 Matrix:\n");
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            scanf("%d", &mat[i][j]);
-    printf("\nCalculating row and column sums...\n");
-    printRowColumnSum(mat);
-    return 0;
-}
-
+ #include <iostream>
+ using namespace std;
+ int main() {
+ int a, n, m;
+ cin >> n >> m;
+ for (int i = 0; i < n; i++)
+ for (int j = 0; j < m; j++)
+ cin >> a[i][j];
+ for (int i = 0; i < n; i++) {
+ int s = 0;
+ for (int j = 0; j < m; j++)
+ s += a[i][j];
+ cout << "Row " << i << " sum " << s << endl;
+ }
+ for (int j = 0; j < m; j++) {
+ int s = 0;
+ for (int i = 0; i < n; i++)
+ s += a[i][j];
+ cout << "Col " << j << " sum " << s << endl;
+ }
+ return 0;
+ }
+ i
